cebbd1afe0a3bdd3be190c7758d2191d3c6ef35a
laoboli <1293528695@qq.com>
2023-08-26 16:51:41 +0800

    Interpreter.

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -11,7 +11,10 @@ import java.util.List;
 
 public class Lox {
 
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    private static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -30,6 +33,9 @@ public class Lox {
         // Indicate an error in the exit code.
         if (hadError) {
             System.exit(65);
+        }
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -57,7 +63,8 @@ public class Lox {
         if (hadError)
             return;
 
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
+
     }
 
     static void error(int line, String message) {
@@ -77,6 +84,12 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                                   "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
 }
