d5ad8681ca51477e67d3407007bf90434bab6071
laoboli <1293528695@qq.com>
2023-08-28 20:17:36 +0800

    closure.

--- src/lox/LoxFunction.java
+++ src/lox/LoxFunction.java
@@ -4,16 +4,19 @@ import java.util.List;
 
 public class LoxFunction implements LoxCallable {
 
+    private final Environment closure;
+
     private final Stmt.Function declaration;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration,Environment closure) {
         this.declaration = declaration;
+        this.closure = closure;
     }
 
     @Override
     public Object call(Interpreter interpreter,
                        List<Object> arguments) {
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             environment.define(declaration.params.get(i).lexeme,
                                arguments.get(i));
